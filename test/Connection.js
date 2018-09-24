a1a0aceb10ba62b37e5cab79a47370a3d2b4a9c5
Derick Cardin <derickc50@gmail.com>
2018-09-24 23:00:54 +0000

    Lesson 6

--- test/Connection.js
+++ test/Connection.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
-//Connect to mongodb
+
+//ES6 Promises
+mongoose.Promise = global.Promise;
+
+//connect to the db before tests run
+before(function(done){
+  //Connect to mongodb
 mongoose.connect('mongodb://localhost/testaroo');
 
 mongoose.connection.once('open', function(){
    console.log('Connection has been made. now make fireworks...'); 
-
+done();
 }).on('error', function(error){
     console.log('Connection error:', error);
+});  
 });
+
+
+
