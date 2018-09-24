d69404a35b752b8f30935299ced49196d775677d
Derick Cardin <derickc50@gmail.com>
2018-09-24 23:08:27 +0000

    Final Tutorial

--- test/Connection.js
+++ test/Connection.js
@@ -7,7 +7,7 @@ mongoose.Promise = global.Promise;
 //connect to the db before tests run
 before(function(done){
   //Connect to mongodb
-mongoose.connect('mongodb://localhost/testaroo');
+mongoose.connect('mongodb://test:testing1@ds259732.mlab.com:59732/derickcardin');
 
 mongoose.connection.once('open', function(){
    console.log('Connection has been made. now make fireworks...'); 
