6d1b74781a7ded51f0e69b1ce2a7f7dd6a80b65c
Derick Cardin <derickc50@gmail.com>
2018-09-25 21:45:25 +0000

    Tutoriutorial 14

--- test/updating_test.js
+++ test/updating_test.js
@@ -8,7 +8,8 @@ var char;
 
    beforeEach(function(done){
        char = new MarioChar({
-       name: 'Mario'
+       name: 'Mario',
+       weight: 50
    });
    
    char.save().then(function(){
@@ -24,6 +25,16 @@ it('updates one record from the database', function(done){
     MarioChar.findOneAndUpdate({name: 'Mario'}, {name: 'Luigi'}).then(function(){
        MarioChar.findOne({_id: char._id}).then(function(result){
            assert(result.name === 'Luigi');
+           done();
+       }) ;
+    });
+   
+}); 
+
+it('increments the weight by 1', function(done){
+    MarioChar.update({}, {$inc: {weight: 1} }).then(function(){
+       MarioChar.findOne({name: 'Mario'}).then(function(record){
+           assert(record.weight === 51);
            done();
        }) ;
     });
